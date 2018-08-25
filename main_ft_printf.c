0acded623a822a454c414c236032c43c181e543c
Ctsu27 <kehuang42student@gmail.com>
2018-08-25 10:47:53 +0200

    add repos

---
+++ main_ft_printf.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_ft_printf.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kehuang <kehuang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/25 10:46:47 by kehuang           #+#    #+#             */
+/*   Updated: 2018/08/25 10:46:49 by kehuang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_printf(char const *fmt, ...);
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RET "ret = %d\n"
+
+int		dtest(int (*out)(char const *, ...), char *str, void *varg)
+{
+	int		n;
+
+	n = (int)varg;
+	return (out(str, n));
+}
+
+int		main(int argc, char **argv)
+{
+	int		(*out[2])(char const *, ...) = {&printf, &ft_printf};
+	int		(*test[10])(int (*f)(char const *, ...), char *, void *) = {
+		&dtest,
+//		&utest, &otest, &xtest, &x_test, &ptest,
+//		&ctest,
+//		&stest,
+//		&c_test,
+//		&s_test
+	};
+	int		i;
+	int		start;
+	int		end;
+
+	if (argc != 4) {
+		printf("usage: %s: [0-1] <fmt> string\n", argv[0]);
+		printf("[0-1]: 0 == printf; 1 == ft_printf\n");
+		printf("<fmt>: duoxXpcsCS\n");
+		printf("string: %%d\\n\n");
+		return (-1);
+	}
+	if (argv[2][0] == 'd') {
+		start = -500;
+		end = 500;
+	}
+	else {
+		start = 0;
+		end = 1000;
+	}
+
+	i = atoi(argv[1]);
+	for (int c = start; c < end; c++)
+		out[i](RET, out[i](argv[3], c));
+	return (0);
+}
---
+++ test.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+bin=destroyer
+
+#i = 0
+dtest=(
+"%%d: [%d]"
+"%%d: [%50d]"
+"%%d: [%4d]"
+"%%d: [%3d]"
+"%%d: [%2d]"
+"%%d: [%1d]"
+"%%d: [%0d]"
+"%%d: [%01d]"
+"%%d: [%02d]"
+"%%d: [%03d]"
+"%%d: [%04d]"
+"%%d: [%040d]"
+"%%d: [%.0d]"
+"%%d: [%.d]"
+"%%d: [%.3d]"
+"%%d: [%.10d]"
+"%%d: [%.40d]"
+"%%d: [%40.40d]"
+"%%d: [%40.38d]"
+"%%d: [%38.40d]"
+"%%d: [%1.4d]"
+"%%d: [%4.1d]"
+"%%d: [%18.0d]"
+"%%d: [%1.0d]"
+"%%d: [%01.0d]"
+"%%d: [%018.0d]"
+"%%d: [%010.20d]"
+"%%d: [%010.10d]"
+)
+
+#echo ${dtest[0]}
+#echo ${dtest[1]}
+#echo ${dtest[2]}
+
+#	av[1] == pf || ft_pf
+#	av[2] == str test
+
+
+for str in "${dtest[@]}"
+do
+	./$bin 1 $str > rl_pf;
+	./$bin 0 $str > ft_pf;
+	diff -s rl_pf ft_pf;
+done
+
+
+#./a.out 1 stest[0] >> rl_pf
+#./a.out 0 stest[0] >> ft_pf
