5ce67ed4633196ea0e3370c9e4eadd05edd4f584
Ctsu27 <kehuang42student@gmail.com>
2018-09-14 12:51:01 +0200

    nealy done; need to add %s test and eventually change the for loops in only 2 loops with a double list travel

--- main_ft_printf.c
+++ main_ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kehuang <kehuang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/25 10:46:47 by kehuang           #+#    #+#             */
-/*   Updated: 2018/08/25 13:21:29 by kehuang          ###   ########.fr       */
+/*   Updated: 2018/09/14 12:39:34 by kehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,31 @@ int		ft_printf(char const *fmt, ...);
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define RET "ret = %d\n"
+#define SCONVERTER "duoxXpcs"
 
-int		dtest(int (*out)(char const *, ...), char *str, void *varg)
-{
-	long	n;
+int		get_cvt(char *s) {
+	char const	*sconv = SCONVERTER;
+	char	*pos;
 
-	n = (long)varg;
-	return (out(str, n));
+	if (s[0] == '\0' || s[1] != '\0'
+			|| ((pos = strchr(sconv, s[0])) == NULL))
+		return (-1);
+	return ((int)(pos - sconv));
 }
 
-int		utest(int (*out)(char const *, ...), char *str, void *varg)
-{
-	unsigned long	n;
-
-	n = (unsigned long)varg;
-	return (out(str, n));
+int		dtest(int (*out)(char const *, ...), char *str, void *varg) {
+	return (out(str, (long)varg));
 }
 
-int		otest(int (*out)(char const *, ...), char *str, void *varg)
-{
-	unsigned long	n;
-
-	n = (unsigned long)varg;
-	return (out(str, n));
+int		utest(int (*out)(char const *, ...), char *str, void *varg) {
+	return (out(str, (unsigned long)varg));
 }
 
-int		xtest(int (*out)(char const *, ...), char *str, void *varg)
-{
-	unsigned long	n;
-
-	n = (unsigned long)varg;
-	return (out(str, n));
-}
-
-int		x_test(int (*out)(char const *, ...), char *str, void *varg)
-{
-	unsigned long	n;
-
-	n = (unsigned long)varg;
-	return (out(str, n));
-}
-
-int		ptest(int (*out)(char const *, ...), char *str, void *varg)
-{
-	unsigned long	n;
-
-	n = (unsigned long)varg;
-	return (out(str, n));
+int		ctest(int (*out)(char const *, ...), char *str, void *varg) {
+	return (out(str, (char)varg));
 }
 
 int		main(int argc, char **argv)
@@ -70,16 +46,18 @@ int		main(int argc, char **argv)
 	int		(*out[2])(char const *, ...) = {&printf, &ft_printf};
 	int		(*test[10])(int (*f)(char const *, ...), char *, void *) = {
 		&dtest,
-		&utest, &otest, &xtest, &x_test, &ptest,
-//		&ctest,
+		&utest, &utest, &utest, &utest, &utest,
+		&ctest,
 //		&stest,
 //		&c_test,
 //		&s_test
 	};
+	int		fconverter_idx;
 	int		i;
 	int		start;
 	int		end;
 
+//	usage
 	if (argc != 4) {
 		printf("usage: %s: [0-1] <fmt> string\n", argv[0]);
 		printf("[0-1]: 0 == printf; 1 == ft_printf\n");
@@ -87,17 +65,46 @@ int		main(int argc, char **argv)
 		printf("string: %%d\\n\n");
 		return (-1);
 	}
+
+//	get the converter test function
+	if ((fconverter_idx = get_cvt(argv[2])) < 0) {
+		printf("Not a valid converter\n");
+		return (-1);
+	}
+
+//	delim test for integer
 	if (argv[2][0] == 'd') {
 		start = -500;
 		end = 500;
+	}
+	else if (argv[2][0] == 'c') {
+		start = -300;
+		end = 300;
 	}
 	else {
 		start = 0;
 		end = 1000;
 	}
 
+//	get the function 0 == printf :: 1 == ft_printf
 	i = atoi(argv[1]);
-	for (int c = start; c < end; c++)
-		out[i](RET, out[i](argv[3], c));
+	if (i > 1)
+		i = 1;
+	if (i < 0)
+		i = 0;
+
+//	launching the test
+	if (fconverter_idx < 7) {
+		for (int c = start; c < end; c++)
+			out[i](RET, out[i](argv[3], c));
+		out[i](RET, out[i](argv[3], -2147483648));
+		out[i](RET, out[i](argv[3], 2147483647));
+		out[i](RET, out[i](argv[3], -2147483649));
+		out[i](RET, out[i](argv[3], 2147483648));
+		out[i](RET, out[i](argv[3], 4294967296));
+		out[i](RET, out[i](argv[3], 4294967297));
+	}
+	else
+		out[i](RET, out[i](argv[3], 42));
 	return (0);
 }
